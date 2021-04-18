74e0663d65eec96f385fb0c7a78266581a6efeb8
Mohammed Sohail <sohailsameja@gmail.com>
2021-04-18 13:58:29 +0300

    db: filtered tweets

---
+++ database/migrations/20210418105611_filtered_tweets.js
@@ -0,0 +1,34 @@
+exports.up = (knex) => {
+  return knex.schema
+    .createTable("categories", (table) => {
+      table.increments("id").primary();
+      table.string("catgeory_name").unique();
+      table.timestamps(true, true);
+    })
+    .createTable("filtered_tweets", (table) => {
+      table.increments("id").primary();
+      table
+        .integer("source")
+        .unsigned()
+        .references("id")
+        .inTable("sources")
+        .onDelete("CASCADE")
+        .index();
+      table
+        .integer("category")
+        .unsigned()
+        .references("id")
+        .inTable("categories")
+        .onDelete("CASCADE")
+        .index();
+      table.string("tweet_id");
+      table.string("tweet_date");
+      table.timestamps(true, true);
+    });
+};
+
+exports.down = (knex) => {
+  return knex.schema
+    .dropTableIfExists("filtered_tweets")
+    .dropTableIfExists("categories");
+};
